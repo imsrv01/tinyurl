92fe8d1eee4914de42d008972e9b5a48f860f801
Ubuntu <ubuntu@ip-172-31-20-72.us-west-2.compute.internal>
2019-05-03 22:23:15 +0000

    new files

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,17 +1,30 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
-from helloworld.flaskrun import flaskrun
+from flask import Flask, Response, render_template, request, redirect
+#from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
 
-@application.route('/', methods=['GET'])
-def get():
-    return Response(json.dumps({'Output': 'Hello RUDRA'}), mimetype='application/json', status=200)
+@application.route('/', methods=['GET', 'POST'])
+def url():
+    form = request.form
+    if request.method == 'POST':
+        longurl = request.form.get('url')
+        print("longurl --> " + longurl)
+        return render_template("home.html", short_url=longurl)
+    return render_template("home.html")
 
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
-
+@application.route('/<short_url>')
+def redirect_short_url(short_url):
+    print("redirecting to  --> " + short_url)
+    return redirect("https://github.com/narenaryan/Pyster/blob/master/templates/home.html")
+  
+    
 if __name__ == '__main__':
-    flaskrun(application)
+    #flaskrun(application)
+    #application.run()
+    application.run(
+        debug="true",
+        host="127.0.0.1",
+        port=8080
+    )
